588bd1631f23ba1806eb6bfc824c977652e46a74
Youssef Fathy <yousseffathy771@gmail.com>
2025-12-21 15:07:04 +0200

    edited class Storage

--- SudokuGame/src/Control/Storage.java
+++ SudokuGame/src/Control/Storage.java
@@ -72,3 +72,63 @@ public class Storage {
         int[][] data = parseFile(target);
         return new Game(data);
     }
+    public void saveGame(Difficulty d, Game g) throws IOException {
+        String p = resolveFolder(d);
+        String n = "sudoku_" + System.currentTimeMillis() + ".txt";
+        File f = new File(p, n);
+        
+        saveToFile(f, g.board);
+    }
+
+    public void saveUnfinishedGame(Game g) throws IOException {
+        File f = new File(F_INC, GAME_NAME);
+        saveToFile(f, g.board);
+    }
+
+    public void logAction(String s) throws IOException {
+        File f = new File(F_INC, LOG_NAME);
+        FileWriter fw = new FileWriter(f, true);
+        fw.write(s + "\n");
+        fw.close();
+    }
+
+    private String resolveFolder(Difficulty d) {
+        if (d == Difficulty.EASY) return F_EASY;
+        if (d == Difficulty.MEDIUM) return F_MED;
+        if (d == Difficulty.HARD) return F_HARD;
+        return F_EASY;
+    }
+
+    private int[][] parseFile(File f) throws IOException {
+        int[][] b = new int[9][9];
+        Scanner sc = new Scanner(f);
+        
+        int r = 0;
+        while (sc.hasNextLine() && r < 9) {
+            String l = sc.nextLine();
+            String[] sp = l.split(",");
+            for (int c = 0; c < 9; c++) {
+                if (c < sp.length) {
+                    b[r][c] = Integer.parseInt(sp[c].trim());
+                } else {
+                    b[r][c] = 0;
+                }
+            }
+            r++;
+        }
+        sc.close();
+        return b;
+    }
+
+    private void saveToFile(File f, int[][] b) throws IOException {
+        PrintWriter pw = new PrintWriter(new FileWriter(f));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                pw.print(b[i][j]);
+                if (j < 8) pw.print(",");
+            }
+            pw.println();
+        }
+        pw.close();
+    }
+}
