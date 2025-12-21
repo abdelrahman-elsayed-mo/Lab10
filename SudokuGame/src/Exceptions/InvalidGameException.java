e8beeeb9d592e78cee834d3a42b80933248ad5b5
Abdelrahman Elsayed <abdosayedmo2005@gmail.com>
2025-12-21 04:11:05 +0200

    Exceptions implementation

---
+++ SudokuGame/src/Exceptions/InvalidGameException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author Abdelrahman Elsayed
+ */
+public class InvalidGameException extends Exception {
+      public InvalidGameException(String message) {
+        super(message);
+    }
+}
---
+++ SudokuGame/src/Exceptions/InvalidSolutionException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exceptions;
+
+/**
+ *
+ * @author Abdelrahman Elsayed
+ */
+public class InvalidSolutionException extends Exception {
+    public InvalidSolutionException(String message) {
+        super(message);
+    }
+}
